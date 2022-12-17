4a5869a8402ed6f89b8cb1212adef77f7c3954b1
Mahinkahn <mahinkahn37@gmail.com>
2022-12-17 13:04:33 +0600

    ste

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -4,17 +4,19 @@ import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    const [isAsc, setIsAsc] = useState(true)
     useEffect(() => {
-        fetch('https://genius-car-server-bice-two.vercel.app/services')
+        fetch(`http://localhost:5000/services?order=${isAsc ? 'asc' : 'desc'}`)
             .then(res => res.json())
             .then(data => setServices(data))
-    }, [])
+    }, [isAsc])
     return (
         <div>
             <div className='text-center mb-6'>
                 <p className="text-2xl font-bold text-orange-600">Service</p>
                 <h2 className='my-5 text-5xl font-bold'>Our Service Area</h2>
                 <p>The majority have suffered alteration in some form, by injected humour, or Randomised <br /> words which don't look even slightly believable. </p>
+                <button className='btn btn-primary mt-2' onClick={() => setIsAsc(!isAsc)}>{isAsc ? 'desc' : 'asc'}</button>
             </div>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {
